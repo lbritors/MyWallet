98c5eadca5caeb54f788f45a64ccbd603e527371
lbritors <britoleticiars@gmail.com>
2023-04-18 23:57:04 -0300

    sign-up request

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient, MongoClient } from "mongodb";
+import { MongoClient} from "mongodb";
 import dotenv from "dotenv";
+import Joi from "joi";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try{
@@ -14,3 +17,39 @@ try{
     console.log(e.message);
 }
 const db = mongoClient.db();
+
+app.post("/cadastro", async(req, res) => {
+    const {nome, email, senha, confirmeSenha} = req.body;
+    const schema = Joi.object({
+        nome: Joi.string().required(),
+        email: Joi.string().email().required(),
+        senha: Joi.string().alphanum().min(3).required(),
+        confirmeSenha: Joi.ref('senha')
+    });
+    
+    const validation = schema.validate(req.body, {abortEarly: false});
+    if(validation.error){
+        const erros = validation.error.details.map(e => e.message);
+        return res.status(422).send(erros);
+
+    }
+
+    const senhaCript = bcrypt.hashSync(senha, 10);
+    
+    
+   
+    try{
+        const user = await db.collection("usuarios").findOne({email: email});
+        if(user) return res.sendStatus(409)
+        
+        await db.collection("usuarios").insertOne({nome, email, senhaCript});
+        res.sendStatus(201);
+
+    }catch(err) {
+        res.status(500).send(err.message);
+    }
+})
+
+
+app.listen(5000, console.log("Listening PORT 5000"));
+
